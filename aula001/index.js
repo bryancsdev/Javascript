70e6f154794f031c3d559234d6973d587b3280b6
bryandev <109782855+bryancsdev@users.noreply.github.com>
2022-11-28 17:03:25 -0300

    aula 1 completa

---
+++ aula001/index.js
@@ -0,0 +1,6 @@
+console.log('Bryan "Campagnole"'); //Strings
+console.log("Bryan 'Campagnole'"); //Strings
+console.log(`'Bryan' "Campagnole"`); //Templates Strings
+console.log(123456); // Literal
+console.log(213.23838) // Float
+console.log(1242, 18327823.1231, 'Bryan Campagnole') //Juntos
