68f493d763558a34b409d93660e410eba557071e
bryandev <109782855+bryancsdev@users.noreply.github.com>
2022-12-05 15:32:40 -0300

    a

---
+++ aula007/index.js
@@ -0,0 +1,10 @@
+var nome = ('Bryan')//NÃO USAR VAR
+var nome = ('Campagnole')// FORMA DE REDECLARAR VARIAVEL COM VAR
+console.log(nome)
+
+let nome2 = ('Bryan')//SEMPRE UTILIZAR LET
+nome2 = ('Campagnole')//REDECLARAR VARIAVEL COM LET
+console.log(nome2)
+
+nome3 = ('Bryan')// SEMPRE DECLARES VARIAVEIS COM CONST OU LET POIS SEM ELES VIRA UMA VARIAVEL GLOBAL
+console.log(nome3)
---
+++ aula008/index.js
@@ -0,0 +1,10 @@
+const nome = ('Bryan')//Strings
+const nome2 = ("Bryan")//Strings
+const nome3 = (`Bryan`)//Strings
+const num = (10)//Number
+const num2 = (10.73)//Number
+let nomeAluno;//Undefined = nao tem valor na memoria
+let sobrenomeAluno = null;// Nulo = nao tem valor na memoria
+const aprovado = true;//boolean = true ou false
+
+console.log(typeof sobrenomeAluno, sobrenomeAluno)
