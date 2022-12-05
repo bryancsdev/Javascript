84a95f77335df15a1905957e3f0dd044f52c3811
bryandev <109782855+bryancsdev@users.noreply.github.com>
2022-12-05 00:01:13 -0300

    Create index.js

---
+++ aula006/index.js
@@ -0,0 +1,15 @@
+/*Luiz Otávio Miranda tem 30 anos, pesa 84 kg
+tem 1.8 de altura e seu IMC é de 25.925925925925924
+Luiz Otávio nasceu em 1980
+*/
+
+
+let nome = ('Bryan Campagnole Silveira')
+let idade = (14)
+let peso = (70)
+let altura = (1.70)
+let imc = peso / altura
+let anoNascimento = idade - 2022
+console.log(nome, 'Tem', idade, 'anos, pesa',peso, 'kg');
+console.log('Tem', altura, 'de altura e seu IMC é de',imc )
+console.log(nome, 'nasceu em',anoNascimento)
